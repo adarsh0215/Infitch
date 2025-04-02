32e8cbefe019b3de34fa7fd66a86d99ac9071d8c
adarshMistEO <adarshyadav0215@gmail.com>
2025-04-02 19:26:57 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export const content = ["./*.html", "./src/**/*.css"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ["Manrope", "sans-serif"],
+      serif: ['Playfair Display', 'serif'],
+    },
+    colors: {
+      'infitch-blue': '#4648CC',
+      'primary': '#4648CC', // Custom color
+    }
+  },
+};
+export const plugins = [];
